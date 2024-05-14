c4299d77cd12b1c84235eeeaf182e6744ebb1ef7
Dimitrije <skipina.dimitrije@gmail.com>
2024-05-14 16:14:16 +0200

    first init

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,47 @@
+<template>
+  <header class="header">
+    <div class="header__content">
+      <div>LOGO</div>
+      <div class="header__nav">
+        <router-link class="header__nav-link" to="/">Home</router-link>
+        <router-link class="header__nav-link" to="/about">About</router-link>
+      </div>
+      <div class="header__right">Login</div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  height: 53px;
+  background-color: bisque;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &__content {
+    width: 1200px;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #46a358;
+  }
+  &__logo {
+    color: #46a358;
+  }
+  &__nav {
+    display: flex;
+    gap: 50px;
+    &-link {
+      font-weight: 700;
+      font-size: 16px;
+      color: #3d3d3d;
+    }
+  }
+  &__rigth {
+    display: flex;
+    gap: 50px;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="home-page"></div>
+</template>
+
+<style lang="scss" scoped>
+.home-page {
+  background-color: rgb(49, 49, 49);
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+</style>
